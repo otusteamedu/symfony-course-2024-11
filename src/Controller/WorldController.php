cfdfa97489a98ea197ae45411bfdd8e7c1d2c52a
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-08-09 15:09:18 +0300

    Add FormatService

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Domain\Service\FormatService;
-use App\Domain\Service\MessageService;
+use App\Domain\Service\GreeterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,14 +11,14 @@ class WorldController extends AbstractController
 {
     public function __construct(
         private readonly FormatService $formatService,
-        private readonly MessageService $messageService,
+        private readonly GreeterService $greeterService,
     )
     {
     }
 
     public function hello(): Response
     {
-        $result = $this->formatService->format($this->messageService->printMessages('world'));
+        $result = $this->formatService->format($this->greeterService->greet('world'));
 
         return new Response("<html><body>$result</body></html>");
     }
