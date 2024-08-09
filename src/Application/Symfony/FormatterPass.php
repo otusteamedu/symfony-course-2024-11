527806b3ab63009d8d01cc8ce25472fa810c02a1
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-08-09 15:50:45 +0300

    Add services with tags and compiler passes

--- src/Application/Symfony/FormatterPass.php
+++ src/Application/Symfony/FormatterPass.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-class GreeterPass implements CompilerPassInterface
+class FormatterPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
@@ -15,9 +15,9 @@ class GreeterPass implements CompilerPassInterface
             return;
         }
         $messageService = $container->findDefinition(MessageService::class);
-        $greeterServices = $container->findTaggedServiceIds('app.greeter_service');
-        foreach ($greeterServices as $id => $tags) {
-            $messageService->addMethodCall('addGreeter', [new Reference($id)]);
+        $formatterServices = $container->findTaggedServiceIds('app.formatter_service');
+        foreach ($formatterServices as $id => $tags) {
+            $messageService->addMethodCall('addFormatter', [new Reference($id)]);
         }
     }
 }
